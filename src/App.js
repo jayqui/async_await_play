bb546350f4ffcbb8ce0d1dbf9b1a9827cdcd496e
Jay Quigley <jay.quigley@omadahealth.com>
2022-01-19 17:04:25 -0800

    Simple fetching from a free API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  async function fetchData() {
+    const response = await fetch('https://api.publicapis.org/entries');
+    const data = await response.json();
+    setEntries(data.entries);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {entries.map((entry, index) => (
+            <li key={index}>{entry.Description}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
